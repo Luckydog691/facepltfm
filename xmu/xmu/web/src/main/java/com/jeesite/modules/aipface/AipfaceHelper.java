293baf5e89f75cb17a3a386d199c811d9694de94
Luckydog691 <1256161580@qq.com>
2021-07-12 11:08:28 +0800

    Update AipfaceHelper.java

--- xmu/xmu/web/src/main/java/com/jeesite/modules/aipface/AipfaceHelper.java
+++ xmu/xmu/web/src/main/java/com/jeesite/modules/aipface/AipfaceHelper.java
@@ -1,6 +1,9 @@
 package com.jeesite.modules.aipface;
 
 import com.baidu.aip.face.AipFace;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class AipfaceHelper
 {
@@ -12,6 +15,12 @@ public class AipfaceHelper
      */
      public void detect(String url)
      {
+         String imageStr = BASE64Util.convertFileToBase64(url);
+         String imageType = "BASE64";
+         AipFace inst = ConnectionSingleton.getInstance();
+         HashMap<String, String> options = new HashMap<String, String>();
+         options.put("face_field", "age,beauty,expression,face_shape,gender,glasses,landmark,landmark72,landmark150,quality,eye_status,emotion,face_type");
+         JSONObject ret = inst.detect(imageStr,imageType,options);
 
      }
 }
