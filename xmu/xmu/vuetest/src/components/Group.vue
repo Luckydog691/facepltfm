d315a81d2e6bc3872c72a62623fc47fb1c7e6092
Luckydog691 <1256161580@qq.com>
2021-07-19 16:21:24 +0800

    7-19

--- xmu/xmu/vuetest/src/components/Group.vue
+++ xmu/xmu/vuetest/src/components/Group.vue
@@ -1,38 +1,86 @@
-<head>
-<meta charset="UTF-8">
-<!-- 引入样式 -->
-<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-default/index.css">
-<script src="https://unpkg.com/vue/dist/vue.js"></script>
-<script src="https://unpkg.com/element-ui/lib/index.js"></script>
-</head>
+
 
 <template>
-  <el-table :data="groups" class="data_table">
-    <el-table-column prop="group_id" label="组名" width="180">
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-dialog
+      title="新建人脸组"
+      :visible.sync="dialogVisible"
+      width="30%"
+      @close="closeDialog"
+    >
+      <el-form :model="groupForm">
+        <el-form-item label="组名称">
+          <el-input v-model="groupForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+            <span class="dialog-footer">
+              <el-button type="primary" @click="postData(groupForm.name)">确 定</el-button>
+            </span>
+      </template>
+    </el-dialog>
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-header style="text-align: right; font-size: 12px">
+        <el-button @click="dialogVisible=true">新建人脸组</el-button>
+      </el-header>
+      <el-table :data="groups" border height="250" class="data_table">
+        <el-table-column prop="group_id" align="center" label="组名" width="640"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template #default="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">查看</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-container>
+  </div>
 </template>
 
+
 <script>
-    export default {
-        name: "groups",
-      data(){
-        return {
-          groups:[
-          ]
-        }
+export default {
+  name: "groups",
+  data(){
+    return {
+      dialogVisible: false,
+      groupForm: {
+        name: ''
       },
-      //页面初始化完成后要加载的东西
-      created() {
-        //this指的是当前的vue对象，把vue对象赋给_this
-        const _this=this
-        this.axios.get('http://localhost:8080/web/sample/aipface/findGroup').then(function (resp) {
-          //回调函数中的this指的是回调
-          //把data数据赋给_this，即vue对象
-          _this.groups=resp.data
-        })
-      }
+      groups:[
+      ]
     }
+  },
+  //页面初始化完成后要加载的东西
+  created() {
+    //this指的是当前的vue对象，把vue对象赋给_this
+    const _this=this
+    this.axios.get('http://localhost:8080/web/sample/aipface/findGroup').then(function (resp) {
+      //回调函数中的this指的是回调
+      //把data数据赋给_this，即vue对象
+      _this.groups=resp.data
+    })
+  },
+  methods:{
+    closeDialog() {
+      this.dialogVisible = false
+    },
+    postData (str) {
+      this.dialogVisible = false
+      this.$http({
+        method: 'post',
+        url: 'http://localhost:8080/web/sample/aipface/addGroup?name='+str,
+        data: {
+        }
+      })
+
+    }
+  }
+}
 </script>
 <style scoped>
 </style>
--- xmu/xmu/web/src/main/java/com/jeesite/modules/aipface/controller/AipfaceController.java
+++ xmu/xmu/web/src/main/java/com/jeesite/modules/aipface/controller/AipfaceController.java
@@ -2,12 +2,12 @@ package com.jeesite.modules.aipface.controller;
 
 import com.jeesite.modules.aipface.entity.Group;
 import com.jeesite.modules.aipface.service.AipfaceFaceSearchHelper;
+import com.jeesite.modules.aipface.service.AipfaceUserOperationHelper;
 import org.jasig.cas.client.authentication.SimpleGroup;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +18,18 @@ public class AipfaceController {
     @Autowired
     AipfaceFaceSearchHelper aipfaceFaceSearchHelper;
 
+    @Autowired
+    AipfaceUserOperationHelper aipfaceUserOperationHelper;
+
     @GetMapping("/findGroup")
     public List<Group> findGroup(){
         ArrayList<Group> ret =  aipfaceFaceSearchHelper.selectGroupList(100);
+        return ret;
+    }
+
+    @PostMapping("/addGroup")
+    public boolean addGroup(@RequestParam(value = "name") String groupName){
+        boolean ret =  aipfaceUserOperationHelper.createGroup(groupName);
         return ret;
     }
 }
